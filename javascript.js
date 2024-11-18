adb8e8f522c35377d42da75beb1cf5a474fadd55
vmumbi653 <[mainamumbi23@gmail.com]>
2024-11-18 13:01:23 +0300

    CREATE FUNCTIONS FOR VARIOUS MATH OPERATORS
    I have created functions for the math operators add, subtract, divide and multiply and tested them out in the console to see if they are functional.

---
+++ javascript.js
@@ -0,0 +1,23 @@
+//addition function
+function addition(num1,num2) {
+    return num1 + num2;
+}
+console.log(addition(1,2));
+
+//subtraction function
+function subtraction(num1, num2) {
+    return num1 - num2;
+}
+console.log(subtraction(10,5));
+
+//multiplication function
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+console.log(multiply(6,3));
+
+//divide function
+function divide(num1, num2) {
+    return num1/num2;
+}
+console.log(divide(6,3));
