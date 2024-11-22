6fe2bc5cfffde770ba40e34b135b7ad106269751
vmumbi653 <[mainamumbi23@gmail.com]>
2024-11-22 13:01:01 +0300

    CHANGE HTML, CSS AND JS FILES
    I have made various structural changes to the HTML and CSS files to change the calculator's design. Furthermore, I have added a function of getOperator() in the JS file to get the operator selected. I have also added a function to clear the display of the calculator. I have further declared a variable to track the steps in the calculation process. The calculator is currently functional, but it is yet to do calculations involving more than two numbers.

--- javascript.js
+++ javascript.js
@@ -1,52 +1,126 @@
+const display = document.getElementById('display');
+
 //variables for operands and operator
 let operand1;
 let operator ;
 let operand2;
+let step = 0;
+
+let result = 0;
+
+//number array for calculations
+let numArray = [];
+let numArray2 = [];
+
+// function for  buttons
+function getNumber(num) {
+    if(step === 0 || step === 1){
+        numArray.push(num); //push the number
+        step = 1;
+        operand1 = Number(numArray.join('')) //merge more than two numbers
+        display.value = operand1;
+    } else if(step === 2) {
+        numArray2.push(num);
+        operand2 = Number(numArray2.join(''));
+        display.value = operand2;
+
+    }
+}
+
+// function to get operator
+function getOperator(opr) {
+    step = 2;
+    operator = opr;
+    // display.value = operand1.join(operator);
+}
 
 //addition function
-function addition(num1,num2) {
-    return num1 + num2;
+function addition() {
+    // operator = '+';
+    return operand1 + operand2;
 }
-console.log(addition(1,2));
 
 //subtraction function
-function subtraction(num1, num2) {
-    return num1 - num2;
+function subtraction() {
+    // operator = '-';
+    return operand1 - operand2;
 }
-console.log(subtraction(10,5));
 
 //multiplication function
-function multiply(num1, num2) {
-    return num1 * num2;
+function multiply() {
+    // operator = '*'
+    return operand1 * operand2;
 }
-console.log(multiply(6,3));
 
 //divide function
-function divide(num1, num2) {
-    return num1/num2;
+function divide() {
+    // operator = '/';
+    return  operand1/operand2;
 }
-console.log(divide(6,3));
+
+
 
 //function operate
-function operate(operator, operand1, operand2) {
-    let result;
-    if(operator == '+') {
-        operator = addition();
-        return operand1 + operand2;
-    } else if(operator == '-') {
-        operator = subtraction();
-        return operand1 - operand2;
-    } else if(operator == '*') {
-        operator = multiply();
-        return operand1 * operand2;
-    } else (operator == '/') 
-        operator = divide();
-        if(operand2 === 0) {
-            console.log('Error! Division by Zero is not allowed');
-        } else {
-            return operand1 / operand2;
-        }
-    
+function operate() {
+    result = 0;
+
+    switch(operator) {
+        case '+':
+            // result = operand1 + operand2;
+            result = addition();
+            display.value = result
+            break;
+        case '-':
+            // result = operand1 - operand2;
+            result = subtraction();
+            display.value = result;
+            break;
+        case '*':
+            // result = operand1 * operand2;
+            result = multiply();
+            display.value = result;
+            break;
+        case '/':
+            // result = operand1 / operand2;
+            result = divide();
+            display.value = result;
+            break;
+        default:
+            display.value = 'Error! Division by Zero Not Allowed!';
+    }
+
+    // if(operator === '+') {
+    //     operator = addition();
+    //     result = operand1 + operand2;
+            // display.value = result;
+    // } else if(operator == '-') {
+    //     operator = subtraction();
+    //     result = operand1 - operand2;
+    //      display.value = result;
+    // } else if(operator == '*') {
+    //     operator = multiply();
+    //     result = operand1 * operand2;
+    //      display.value = result;
+    // } else (operator == '/') 
+    //     operator = divide();
+    //     if(operand2 === 0) {
+    //         display.value = 'Error! Division by Zero is not allowed';
+    //     }
+    //      
+    //         result =  operand1 / operand2;
+    //          display.value = result;
    
 }
-console.log(`${operand1} ${operator} ${operand2}`);
+
+// clear Display function
+
+function clearDisplay() {
+    display.value = 0;
+    operand1 = null;
+    operand2 = null;
+    step = 0;
+    numArray = [];
+    numArray2 = [];
+    result = 0;
+}
+
